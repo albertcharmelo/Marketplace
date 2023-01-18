02abef1b60c50aaa8ca46223a3eb181ed68a0fff
davidermu <davidermu123@gmail.com>
2023-01-18 11:05:39 -0400

    cositas
    cambie el index y cree una carpeta :3

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Ricardo xd' })
+  res.status(200).json({ name: 'David 🤬' })
 }
